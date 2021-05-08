34a5b559a87ec02cdbda7016265648650865e3d7
Jesus Macedo <jesus.macedo@globant.com>
2021-05-08 09:06:02 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'goodMoney';
+}
