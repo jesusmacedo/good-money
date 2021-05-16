9ee896d5e8d2917b1bc65c4b0e0a26c2dca3e1be
Jesus Macedo <jesus.macedo@globant.com>
2021-05-15 21:16:09 -0500

    feat(structure): Created Modules for /landing and /dashboard.

---
+++ src/app/pages/landing/landing/landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gm-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.sass']
+})
+export class LandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
